e73fc30681f3463447f08d6710a148cf8b8a1e35
QuantumCoder <157535720+HanzlaCoding@users.noreply.github.com>
2026-05-03 16:07:36 +0500

    final...

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 // Create a configured Axios instance
 // This gives us a single base URL for all our API calls
 const api = axios.create({
-  baseURL: 'http://localhost:3000/api', // Pointing to our backend running locally
+  baseURL: 'https://theeta-lms-backend.onrender.com/api', // Pointing to our backend running locally
   withCredentials: true, // This allows Axios to automatically send and receive cookies
 });
 
